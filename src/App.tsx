db2429d91a067e42a68d32f4a7108a75c3b95b4a
Şerif Miraç Gögebakan <serifggbkn@gmail.com>
2026-02-24 12:57:02 +0300

    feat: add personal details to homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 function App() {
   return (
-    <div>
+    <div className="container">
       <h1>Web Tasarimi ve Programlama</h1>
       <h2>LAB-1</h2>
       <p>Ad Soyad: [Şerif Miraç Gögebakan]</p>
       <p>Ogrenci No: [230541126]</p>
+      <h3>Kişisel Bilgiler</h3>
+      <p>Bölüm: Bilgisayar Mühendisliği</p>
+      <p>Hobiler: Spor, Yazılım, Kamp</p>
     </div>
   );
 }
