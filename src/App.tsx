1e5708664b7f6e1a49dd9eaf2fce6724724a776a
Şerif Miraç Gögebakan <serifggbkn@gmail.com>
2026-02-24 13:53:21 +0300

    feat: update hobbies with Arduino and Raspberry Pi projects

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,11 @@ function App() {
       <h2>LAB-1</h2>
       <p>Ad Soyad: [Şerif Miraç Gögebakan]</p>
       <p>Ogrenci No: [230541126]</p>
+      <p>Bölüm: Bilgisayar Mühendisliği</p>
+      <p>
+        Hobiler: Rekabetçi ve hikaye tabanlı bilgisayar oyunları oynamak, Arduino ve Raspberry Pi
+        gibi geliştirme kartlarıyla kendi elektronik donanım projelerimi tasarlamak.
+      </p>
     </div>
   );
 }
